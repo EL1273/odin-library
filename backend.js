ad662493ee79cd3371ff33e2197f12e5e7cf786a
Ethan Lin <ethanlin@Ethans-MacBook-Pro.local>
2024-01-08 10:47:46 -0800

    Style page
    - styled the library with css
    - debugged remove functionality

--- backend.js
+++ backend.js
@@ -4,11 +4,12 @@ function Book (name, author, pages, index){
     this.name = name;
     this.author = author;
     this.pages = pages;
-    this.read = false;
+    this.read = true;
     this.index = index;
 }
 
 function addBookToLibrary(n,a,p){
+    console.log(myLibrary.length);
     myLibrary.push(new Book(n,a,p,myLibrary.length));
     console.log(myLibrary);
     displayBooks();
@@ -24,33 +25,44 @@ function displayBooks(){
     myLibrary.forEach((book) => {
         let b = document.createElement('div');
         b.classList.add('book');
-        let name = document.createElement('p');
+        let name = document.createElement('div');
         name.classList.add('name');
         name.textContent = book.name;
         b.appendChild(name);
-        let author = document.createElement('p');
+        let author = document.createElement('div');
         author.classList.add('author');
         author.textContent = book.author;
         b.appendChild(author);
-        let pages = document.createElement('p');
+        let pages = document.createElement('div');
         pages.classList.add('pages');
         pages.textContent = book.pages;
         b.appendChild(pages);
         let read = document.createElement('button');
-        read.classList.add('read');
+        read.classList.add("read");
+        if (book.read){
+            read.textContent = 'Read';
+            read.classList.add('green-button');
+        } else {
+            read.textContent = 'Unread';
+            read.classList.add('red-button')
+        }
         read.addEventListener('click', () => {
-            if (book.read){
-                read.textContent = 'Read';
-            }else {
-                read.textContent = 'Unread';
+            if (!book.read){
+                book.read = true;
+            } else {
+                book.read = false;
             }
             displayBooks();
         });
         b.appendChild(read);
         let removeButton = document.createElement('button');
         removeButton.classList.add('remove');
+        removeButton.textContent = "Remove";
         removeButton.addEventListener('click', () => {
             myLibrary.splice(book.index,1);
+            for(let i = book.index; i < myLibrary.length; i ++){
+                myLibrary[i].index--;
+            }
             displayBooks();
         });
         b.appendChild(removeButton);
@@ -60,17 +72,16 @@ function displayBooks(){
 }
 
 const addPopup = document.querySelector('#add-popup');
+const overlay = document.querySelector('.overlay');
 function showAddPopup() {
     addPopup.classList.add("show");
     addPopup.style.display = "flex";
+    overlay.classList.add("show");
+    overlay.style.display = "block";
   }
 const addBookButton = document.querySelector('#add-book-button');
 addBookButton.addEventListener('click', showAddPopup);
 //add popup button
-
-
-
-
 
 const submitButton = document.querySelector('#submit');
 submitButton.addEventListener("click", (event) => {
@@ -81,6 +92,8 @@ submitButton.addEventListener("click", (event) => {
     resetInputValues();
     addPopup.classList.remove("show");
     addPopup.style.display = "none";
+    overlay.classList.remove("show");
+    overlay.style.display = "none";
     event.preventDefault();
 })
 
@@ -92,8 +105,12 @@ function resetInputValues(){
     document.getElementById("author").placeholder = "Author";
     document.getElementById("num-pages").placeholder = "Pages";
 }
-// window.addEventListener("click", function (event) {
-//   if (event.target == addPopup) {
-//     addPopup.classList.remove("show");
-//   }
-// })
+
+window.addEventListener("click", function (event) {
+  if (event.target === addPopup) {
+    addPopup.classList.remove("show");
+    addPopup.style.display = "none";
+    overlay.classList.remove("show");
+    overlay.style.display = "none";
+  }
+})
